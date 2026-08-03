610a73cac7c4ab37c076d25b134435725e49b432
JulianCamiloMosquera <juliancamilomosquera>
2026-08-03 14:20:31 -0500

    feat(auth): implement AuthGuard for route protection

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
-  imports: [CommonModule]
+  providers: [AuthGuard]
 })
 export class CoreModule { }
