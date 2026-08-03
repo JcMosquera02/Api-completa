e38d2d5d4178b381c393fd80216843ede76c7595
JulianCamiloMosquera <juliancamilomosquera>
2026-08-03 13:39:54 -0500

    feat(auth): implement login method in AuthService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor() { }
+  private readonly API_URL = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.API_URL}/auth/login`, { email, password });
+  }
 }
