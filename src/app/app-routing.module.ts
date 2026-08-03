857dea09814a1276f5805f84cb57f50e3c3f3ded
JulianCamiloMosquera <juliancamilomosquera>
2026-08-03 13:18:04 -0500

    feat(routing): configure lazy loading for auth, tickets, and users modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadchildren: () => import('./features/auth/auth.module').them(m => m.AuthModule)
+  },
+  {
+    path: 'tickets',
+    loadChildren: () => import('.features/tickests/tickest.module')then.(m => m.TicketsModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule),
+    canActivate: [RoleGuard],
+    data: { roles: ['admin'] }
+  },
+  { path: '', redirectTo: '/tickets', pathMatch: 'full' },
+  { path: '**', redirectTo: '/tickets' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
