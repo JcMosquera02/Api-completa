353b786ec8c4784eab5b58f349c8c1d33979addb
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-08-17 06:18:16 -0500

    feat(auth): implement AuthInterceptor for JWT token handling

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
-  providers: [AuthGuard, RoleGuard]
+  providers: [
+    AuthGuard,
+    RoleGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ]
 })
 export class CoreModule { }
