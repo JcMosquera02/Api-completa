48d13248614c5bc174231f605133d1684aa803be
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-08-28 08:51:50 -0500

    fix(login): correct formGroup usage in LoginComponent

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SharedModule } from '../../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
