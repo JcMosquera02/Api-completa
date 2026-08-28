440a3a5d97abc41d17e7d02aeb2658dd2858666b
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-08-28 09:08:40 -0500

    refactor(tickets): replace HttpParams with params object in TicketService

--- src/app/core/services/ticket.service.ts
+++ src/app/core/services/ticket.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -9,14 +9,7 @@ export class TicketService {
   constructor(private http: HttpClient) {}
 
   getTickets(params?: { status?: string; priority?: string; page?: number; limit?: number }) {
-    let httpParams = new HttpParams();
-    if (params) {
-      if (params.status) httpParams = httpParams.append('status', params.status);
-      if (params.priority) httpParams = httpParams.append('priority', params.priority);
-      if (params.page) httpParams = httpParams.append('page', params.page.toString());
-      if (params.limit) httpParams = httpParams.append('limit', params.limit.toString());
-    }
-    return this.http.get(`${this.API_URL}/tickets`, { params: httpParams });
+    return this.http.get(`${this.API_URL}/tickets`, { params });
   }
 
   getTicketById(id: string) {
