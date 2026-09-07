fe7b084a8de605712a874e61d1836aae196ce8f2
Julian Camilo Mosquera <juliancamilomosquera6@gmail.com>
2026-09-07 10:17:53 -0500

    feat(users): add UserListComponent for admin

--- src/app/features/users/user-list/user-list.component.ts
+++ src/app/features/users/user-list/user-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+  styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: any[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.getUsers().subscribe({
+      next: (res: any) => {
+        this.users = res.data || [];
+      },
+      error: (err) => {
+        console.error('Error al cargar usuarios:', err);
+      }
+    });
+  }
 }
